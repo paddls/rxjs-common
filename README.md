1e4c1c7e104e238d0b25954eb0b2f2ca2b8e25ad
Romain MARTINEAU <romain.martineau@witty-services.fr>
2020-01-31 15:26:53 +0100

    feat: init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Rxjs Common
---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  roots: ['./src'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest',
+  },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+};
