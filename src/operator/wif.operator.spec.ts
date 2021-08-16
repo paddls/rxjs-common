9976003f1e5b565e57b862ae8e1927366ca2c489
Romain MARTINEAU <romain.martineau@cooperl.com>
2021-08-16 10:40:36 +0200

    fix: build warning

--- src/operator/wif.operator.spec.ts
+++ src/operator/wif.operator.spec.ts
@@ -1,6 +1,6 @@
 import { TestScheduler } from 'rxjs/testing';
 import { RunHelpers } from 'rxjs/internal/testing/TestScheduler';
-import { from, Observable } from 'rxjs';
+import {from, Observable, of} from 'rxjs';
 import { wif } from './wif.operator';
 
 describe('wif', () => {
@@ -12,7 +12,7 @@ describe('wif', () => {
     }));
   });
 
-  it(`should return 'Less than or equal', 'Less than or equal', 'Greater than'`, () => {
+  it(`should return through function 'Less than or equal', 'Less than or equal', 'Greater than'`, () => {
     testScheduler.run(({expectObservable}: RunHelpers) => {
 
       const source$: Observable<string> = from([1, 2, 3]).pipe(
@@ -20,6 +20,61 @@ describe('wif', () => {
           (value: number) => value > 2,
           () => 'Greater than',
           () => 'Less than or equal'
+        )
+      );
+      expectObservable(source$).toBe('(bba|)', {a: 'Greater than', b: 'Less than or equal'});
+    });
+  });
+
+  it(`should return through function 1, 2, 'Greater than'`, () => {
+    testScheduler.run(({expectObservable}: RunHelpers) => {
+
+      const source$: Observable<string> = from([1, 2, 3]).pipe(
+        wif(
+          (value: number) => value > 2,
+          () => 'Greater than'
+        )
+      );
+      expectObservable(source$).toBe('(bca|)', {a: 'Greater than', b: 1, c: 2});
+    });
+  });
+
+  it(`should directly return 'Less than or equal', 'Less than or equal', 'Greater than'`, () => {
+    testScheduler.run(({expectObservable}: RunHelpers) => {
+
+      const source$: Observable<string> = from([1, 2, 3]).pipe(
+        wif(
+          (value: number) => value > 2,
+            'Greater than',
+            'Less than or equal'
+        )
+      );
+      expectObservable(source$).toBe('(bba|)', {a: 'Greater than', b: 'Less than or equal'});
+    });
+  });
+
+  it(`should return observable 'Less than or equal', 'Less than or equal', 'Greater than'`, () => {
+    testScheduler.run(({expectObservable}: RunHelpers) => {
+
+      const source$: Observable<string> = from([1, 2, 3]).pipe(
+        wif(
+          (value: number) => value > 2,
+            of('Greater than'),
+            of('Less than or equal')
+        )
+      );
+      expectObservable(source$).toBe('(bba|)', {a: 'Greater than', b: 'Less than or equal'});
+    });
+  });
+
+  it(`should return through function observable 'Less than or equal', 'Less than or equal', 'Greater than'`, () => {
+    testScheduler.run(({expectObservable}: RunHelpers) => {
+
+      const source$: Observable<string> = from([1, 2, 3]).pipe(
+        wif(
+          (value: number) => value > 2,
+            () => of('Greater than'),
+            () => of('Less than or equal')
         )
       );
       expectObservable(source$).toBe('(bba|)', {a: 'Greater than', b: 'Less than or equal'});
